a98866ef328d1755cb23fd66f6f9b145d2a852dc
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-07-25 13:34:59 +0000

    refactor: rewrite Car constructor implementation
    Using the shorthand notation

--- src/car.ts
+++ src/car.ts
@@ -1,11 +1,5 @@
 export default class Car {
-    private readonly make: string
-    private readonly model: string
-
-    constructor(make: string, model: string) {
-        this.make = make
-        this.model = model
-    }
+    constructor(private readonly make: string, private readonly model: string) {}
 
     startEngine() {
         console.log(`My ${this} has started`)
